ec51695606a698a55033e4fb996ce1a496d9062a
EveryGameSPlay <repsbro@gmail.com>
2020-04-24 14:08:44 +0300

    Input changed. BaseState control refactoring

--- Assets/Scripts/Core/DeviceInput.cs
+++ Assets/Scripts/Core/DeviceInput.cs
@@ -39,75 +39,111 @@ namespace Core
         }
 
         /// <summary>
-        /// Получение значений ввода удерживания по горизонтальной оси от -1 до 1
+        /// Получение значений ввода удерживания по горизонтальной оси от -1 до 1.
+        /// Ноль обозначает отсутствие удерживания.
         /// </summary>
         /// <returns></returns>
         public static float GetHorizontalAxis()
         {
-            float left = Input.GetKey(map.HorizontalLeft) ? -1f : 0f;
-            float right = Input.GetKey(map.HorizontalRight) ? 1f : 0f;
+            float left = Input.GetKey(map.HorizontalLeft) != Input.GetKeyDown(map.HorizontalLeft) ? -1f : 0f;
+            if (left != 0)
+                return left;
 
-            return left + right;
+            float right = Input.GetKey(map.HorizontalRight) != Input.GetKeyDown(map.HorizontalRight) ? 1f : 0f;
+            if (right != 0)
+                return right;
+
+            return 0;
         }
 
         /// <summary>
         /// Получение значений ввода нажатия по горизонтальной оси от -1 до 1
+        /// Ноль обозначает отсутствие нажатия.
         /// </summary>
         /// <returns></returns>
         public static float GetHorizontalAxisDown()
         {
             float left = Input.GetKeyDown(map.HorizontalLeft) ? -1f : 0f;
-            float right = Input.GetKeyDown(map.HorizontalRight) ? 1f : 0f;
+            if (left != 0)
+                return left;
 
-            return left + right;
+            float right = Input.GetKeyDown(map.HorizontalRight) ? 1f : 0f;
+            if (right != 0)
+                return right;
+
+            return 0;
         }
 
         /// <summary>
         /// Получение значений ввода отжатия по горизонтальной оси от -1 до 1
+        /// Ноль обозначает отсутствие отжатия
         /// </summary>
         /// <returns></returns>
         public static float GetHorizontalAxisUp()
         {
             float left = Input.GetKeyUp(map.HorizontalLeft) ? -1f : 0f;
-            float right = Input.GetKeyUp(map.HorizontalRight) ? 1f : 0f;
+            if (left != 0)
+                return left;
 
-            return left + right;
+            float right = Input.GetKeyUp(map.HorizontalRight) ? 1f : 0f;
+            if (right != 0)
+                return right;
+
+            return 0;
         }
 
         /// <summary>
         /// Получение значений ввода удерживания по вертикальной оси от -1 до 1
+        /// Ноль обозначает отсутствие удерживания.
         /// </summary>
         /// <returns></returns>
         public static float GetVerticalAxis()
         {
-            float down = Input.GetKey(map.VerticalDown) ? -1f : 0f;
-            float up = Input.GetKey(map.VerticalUp) ? 1f : 0f;
+            float down = Input.GetKey(map.VerticalDown) != Input.GetKeyDown(map.VerticalDown) ? -1f : 0f;
+            if (down != 0)
+                return down;
 
-            return down + up;
+            float up = Input.GetKey(map.VerticalUp) != Input.GetKeyDown(map.VerticalUp) ? 1f : 0f;
+            if (up != 0)
+                return up;
+
+            return 0;
         }
 
         /// <summary>
         /// Получение значений ввода нажатия по вертикальной оси от -1 до 1
+        /// Ноль обозначает отсутствие нажатия.
         /// </summary>
         /// <returns></returns>
         public static float GetVerticalAxisDown()
         {
             float down = Input.GetKeyDown(map.VerticalDown) ? -1f : 0f;
-            float up = Input.GetKeyDown(map.VerticalUp) ? 1f : 0f;
+            if (down != 0)
+                return down;
 
-            return down + up;
+            float up = Input.GetKeyDown(map.VerticalUp)? 1f : 0f;
+            if (up != 0)
+                return up;
+
+            return 0;
         }
 
         /// <summary>
         /// Получение значений ввода отжатия по вертикальной оси от -1 до 1
+        /// Ноль обозначает отсутствие отжатия
         /// </summary>
         /// <returns></returns>
         public static float GetVerticalAxisUp()
         {
             float down = Input.GetKeyUp(map.VerticalDown) ? -1f : 0f;
-            float up = Input.GetKeyUp(map.VerticalUp) ? 1f : 0f;
+            if (down != 0)
+                return down;
 
-            return down + up;
+            float up = Input.GetKeyUp(map.VerticalUp) ? 1f : 0f;
+            if (up != 0)
+                return up;
+
+            return 0;
         }
 
         /// <summary>
--- Assets/Scripts/Player/BaseState.cs
+++ Assets/Scripts/Player/BaseState.cs
@@ -24,89 +24,52 @@ namespace Player.PlayerStates
         public override PlayerState UpdateState(UpdateData updateData)
         {
                 
-            // Нажатие и удерживание могут совпадать (особенность движка)
-            // Нажатие на кнопку
-            var horDown = (int)DeviceInput.GetHorizontalAxisDown();
-            var verDown = (int)DeviceInput.GetVerticalAxisDown();
-            
             // Удерживание кнопки
             var hor = (int)DeviceInput.GetHorizontalAxis();
             var ver = (int)DeviceInput.GetVerticalAxis();
 
-            
-
+            // Нажатие на кнопку Z
             if (DeviceInput.GetHandleButtonDown())
             {
-                var pos = Vector3.zero;
 
-                // Добавить ориентацию в игрока и по ней лайнкастить
             }
 
-            // Если движемся или двигались только в одну сторону или только нажали
-            if ((Mathf.Abs(hor) + Mathf.Abs(ver)) == 1)
+            // Если есть удержание по одной из осей
+            if (hor != 0 || ver != 0)
             {
+                // Если персонаж должен сдвинутся и он стоит на месте
+                if ((hor != 0 || ver != 0) && Player.IsMoving == false)
+                {
+                    Player.Move(hor, ver);
+                }
+
+            }
+            else
+            {
+                // Нажатие на кнопку
+                var horDown = (int)DeviceInput.GetHorizontalAxisDown();
+                var verDown = (int)DeviceInput.GetVerticalAxisDown();
+
                 var pos = Vector3.zero;
 
-                if(hor != 0)
+                // Учитывается одновременное нажатие
+                if (horDown != 0)
                 {
-                    // Это означает первое нажатие
-                    if(hor == horDown)
-                    {
-                        //Player.ChangeOrientation(hor, ver);
+                    pos = Player.transform.position + Player.transform.right * Player.MoveStep * horDown;
 
-                        pos = Player.transform.position + Player.transform.right * Player.MoveStep * horDown;
-                        // Ищем объект перед нами
-                        var interior = Player.GetComponentByLinecast<Interior>(pos);
-                        if (interior != null)
-                        {
-                            #region TableAvoidance
-                            //TODO Вообще весь следующий блок должен работать только если interior == Table
-                            // Нужно чтобы вызывался метод у наследников Interior.Так как в случае стула у нас pos
-                            // будет равный стулу, разные анимации, вобщем поведение зависит от реализации. 
-
-                            // -> Этот обход стола вообще непонятно зачем существует, ошибка геймдизайна игры
-                            // -> Георгий сказал, что пока не нужно (22.04.2020)
-
-                            // Если не лицом к столу - разверуться
-                            //if (lastHor != hor)
-                            //{
-                            //    lastHor = hor;
-                            //    lastVer = ver;
-                            //    return this;
-                            //}
-                            //Иначе обойти стол
-                            //Debug.Log(interior);
-
-                            //var tmp = hor;
-                            //hor = ver;
-                            //ver = tmp;
-                            #endregion
-                            return this;
-                        }
-                    }
-                }
-                else if(ver != 0)
+                }else if(verDown != 0)
                 {
-                    // Это означает первое нажатие
-                    if (ver == verDown)
-                    {
-                        //Player.ChangeOrientation(hor, ver);
-
-                        pos = Player.transform.position + Player.transform.up * Player.MoveStep * verDown;
-                        // Ищем объект перед нами
-                        var interior = Player.GetComponentByLinecast<Interior>(pos);
-                        if (interior != null)
-                        {
-                            return this;
-                        }
-                    }
+                    pos = Player.transform.position + Player.transform.up * Player.MoveStep * verDown;
                 }
-            }
 
-            // Если персонаж должен сдвинутся и он стоит на месте
-            if ((hor != 0 || ver != 0) && Player.IsMoving == false)
-            {
-                Player.Move(hor, ver);
+                Player.ChangeOrientation(horDown, verDown);
+
+                // Ищем объект перед нами
+                var interior = Player.GetComponentByLinecast<Interior>(pos);
+                if (interior != null)
+                {
+                    return this;
+                }
             }
 
             return this;
